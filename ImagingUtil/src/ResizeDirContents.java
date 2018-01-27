9a1abbc3b57497624d73577565a92ee61995eead
ameenmohamed <ameen.mohammed@hmhco.com>
2018-01-27 18:31:54 +0000

    first Commit

---
+++ ImagingUtil/src/ResizeDirContents.java
@@ -0,0 +1,14 @@
+
+public class ResizeDirContents {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		FileReSize fs = new FileReSize("E:/root/amdata/Rohingya/images/jpg","E:/root/amdata/Rohingya/imgtt");
+		
+
+	}
+
+}
